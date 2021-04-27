3ff8cbd8ba63c38bb12fb0126cfa71d759fca84a
Tyler Grigg <tylerjgrigg@gmail.com>
2021-04-27 10:17:51 -0400

    Corrected dynamics
    Fixed issue where runge_kutta4 would sometimes choose to take zero steps.
    Added output of the state derivative estimate from runge_kutta4
    Added basic plots of rover motion

--- rover-navigation/main_slam.py
+++ rover-navigation/main_slam.py
@@ -17,6 +17,7 @@ def init_sim():
     # Rover Dynamics
     sim_params['rover'] = dynamics.init_rover()
     # Sensors
+    sim_params['sensors']['gyroscope'] = sensors.init_gyroscope()
     # sim_params['sensors']['gyro'] = sensors.init_gyro()
     # Environment
     sim_params['environment'] = environment.init_circular_table()
@@ -47,6 +48,43 @@ def write_sim_output(t, sim_out, env, measurements, estimate, cmd, rover_state):
     return sim_out
 
 
+def plot_rover_states(sim_out):
+    time = sim_out['time']
+    rover_state = np.array(sim_out['rover_state'])
+    x, y, vbx, psi, r, w_l, w_r = (rover_state[:, 0], rover_state[:, 1], rover_state[:, 2], rover_state[:, 3],
+                                   rover_state[:, 4], rover_state[:, 5], rover_state[:, 6])
+    # Rover XY Trajectory
+    fig, ax = plt.subplots()
+    ax.plot(y, x)
+    ax.set_title('Rover Trajectory')
+    ax.set_xlabel('Y (m)')
+    ax.set_ylabel('X (m)')
+    # Rover Position vs. Time
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(time, x)
+    ax[0].set_title('Rover Trajectory')
+    ax[0].set_ylabel('X (m)')
+    ax[1].plot(time, y)
+    ax[1].set_ylabel('Y (m)')
+    ax[1].set_xlabel('Time (s)')
+    # Rover Tangential Velocity vs. Time
+    fig, ax = plt.subplots()
+    ax.plot(time, vbx)
+    ax.set_title('Rover Body X Velocity')
+    ax.set_ylabel('Vbx (m/s)')
+    ax.set_xlabel('Time (s)')
+    # Rover Angular Position & Velocity vs. Time
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(time, psi)
+    ax[0].set_title('Rover Angular States')
+    ax[0].set_ylabel('Psi (m)')
+    ax[1].plot(time, r)
+    ax[1].set_ylabel('r (m)')
+    ax[1].set_xlabel('Time (s)')
+    plt.show()
+    pass
+
+
 # Main -----------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     sim_params = init_sim()
@@ -57,6 +95,7 @@ if __name__ == "__main__":
     rover_state = sim_params['rover']['ics']
     estimate = navigation.init_slam()
     sim_out = None  # Initialize as empty
+    # sim_out = write_sim_output(t, sim_out, env, measurements, estimate, cmd, rover_state)
     # Run simulation
     for t in times:
         env = environment.circular_table(t, sim_params)
@@ -67,3 +106,4 @@ if __name__ == "__main__":
         sim_out = write_sim_output(t, sim_out, env, measurements, estimate, cmd, rover_state)
     # Print/store outputs
     print(rover_state)
+    plot_rover_states(sim_out)
