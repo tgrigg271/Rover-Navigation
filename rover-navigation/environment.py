045b679b7ccb6359495144c6bf2928b7193f9d12
Tyler Grigg <tylerjgrigg@gmail.com>
2021-05-02 15:57:03 -0400

    Updated tag ID Generation
    AprilTag/Marker IDs are now generated without replacement. Duplicate tag IDs would have messed up the SLAM solution.

--- rover-navigation/environment.py
+++ rover-navigation/environment.py
@@ -16,13 +16,16 @@ def init_circular_table(radius=10, n_markers=10):
     env_params['n_markers'] = n_markers
     # Generate camera markers
     markers = []
+    rng = np.random.default_rng()
+    ids = rng.choice(586, size=n_markers, replace=False)  # Tag family 36h11.
+    ids = list(ids)
     for i_marker in range(n_markers):
         r = np.random.uniform(radius)
         theta = np.random.uniform(2*np.pi)
         x, y = (r*np.cos(theta), r*np.sin(theta))
         position = np.array([[x], [y]])
-        id = np.random.randint(586)  # Tag family 36h11. Should this draw without replacement?
-        markers.append(make_april_tag(position, id))
+        tag_id = ids.pop()
+        markers.append(make_april_tag(position, tag_id))
     env_params['markers'] = markers
     return env_params
 
